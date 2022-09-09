7e614a554860487459a7a76093e319b5c3cc8783
Moustafa <moustafaism013@hotmail.com>
2022-09-09 13:35:11 +0300

    Added all apis requests and their display an adjusted some styles

--- scripts/script.js
+++ scripts/script.js
@@ -2,6 +2,9 @@
 const dogImg = document.querySelector("#dog-img");
 const userName = document.querySelector("#user-name");
 const submitBtn = document.querySelector("#submit-btn");
+const genderDisplay = document.querySelector("#gender-display");
+const ageDisplay = document.querySelector("#age-display");
+const nationalityDisplay = document.querySelector("#nationality-display");
 
 // set the source of the image
 fetch('https://dog.ceo/api/breeds/image/random').then((response) => response.json()).then((data) => dogImg.src = data.message);
@@ -14,5 +17,23 @@ submitBtn.addEventListener("click", submitUserName);
 // Functions
 function submitUserName() {
     let name = userName.value;
-
+    if (name != "") {
+        fetch("https://api.genderize.io?name=" + name)
+            .then((response) => response.json())
+            .then((data) => genderDisplay.textContent = data.gender);
+        fetch("https://api.agify.io/?name=" + name)
+            .then((response) => response.json())
+            .then((data) => ageDisplay.textContent = data.age);
+        fetch("https://api.nationalize.io/?name=" + name)
+            .then((response) => response.json())
+            .then((data) => {
+                if (data.country.length > 0) {
+                    if (data.country.length < 2) {
+                        nationalityDisplay.textContent = data.country[0];
+                    } else {
+                        nationalityDisplay.textContent = data.country[0].country_id + ", " + data.country[1].country_id;
+                    }
+                }
+            });
+    }
 }
