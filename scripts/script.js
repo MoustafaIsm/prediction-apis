3e84784c77d48211496a83f01e01090050908d04
Moustafa <moustafaism013@hotmail.com>
2022-09-09 12:56:33 +0300

    Adjusted some responsiveness and added new javascript

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,18 @@
+// Variables
 const dogImg = document.querySelector("#dog-img");
+const userName = document.querySelector("#user-name");
+const submitBtn = document.querySelector("#submit-btn");
+
+// set the source of the image
+fetch('https://dog.ceo/api/breeds/image/random').then((response) => response.json()).then((data) => dogImg.src = data.message);
+
+// Submit button
+submitBtn.addEventListener("click", submitUserName);
 
 
-let imgSrc = fetch('https://dog.ceo/api/breeds/image/random').then((response) => response.json()).then((data) => dogImg.src = data.message);
 
+// Functions
+function submitUserName() {
+    let name = userName.value;
 
-
+}
