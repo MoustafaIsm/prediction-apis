4194a63164494a622d842d345782ff751bc19714
Moustafa <moustafaism013@hotmail.com>
2022-09-11 14:02:37 +0300

    Made the sign up form a pop up

--- scripts/loginScript.js
+++ scripts/loginScript.js
@@ -3,12 +3,16 @@ const dogImg = document.querySelector("#dog-img");
 const signupForm = document.querySelector("#signup-form");
 const signupControl = document.querySelector("#signup-control");
 const loginBtn = document.querySelector("#login-btn");
+const span = document.querySelector("#close-wrapper");
 
 // set the source of the image
 fetch('https://dog.ceo/api/breeds/image/random').then((response) => response.json()).then((data) => dogImg.src = data.message);
 
 // Signup control
 signupControl.addEventListener("click", toggleSignupForm);
+// Span button
+span.addEventListener("click", toggleSignupForm)
+// Login button
 loginBtn.addEventListener("click", loginUser);
 
 // Functions
