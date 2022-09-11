ea2fef91ec91c0c2cbc6b372cf10b1db9369dd6e
Moustafa <moustafaism013@hotmail.com>
2022-09-11 13:35:22 +0300

    Added an image to the page

--- scripts/loginScript.js
+++ scripts/loginScript.js
@@ -1,16 +1,18 @@
 // Variables
+const dogImg = document.querySelector("#dog-img");
 const signupForm = document.querySelector("#signup-form");
 const signupControl = document.querySelector("#signup-control");
 const loginBtn = document.querySelector("#login-btn");
 
+// set the source of the image
+fetch('https://dog.ceo/api/breeds/image/random').then((response) => response.json()).then((data) => dogImg.src = data.message);
+
 // Signup control
 signupControl.addEventListener("click", toggleSignupForm);
-// Login button
 loginBtn.addEventListener("click", loginUser);
 
 // Functions
 function toggleSignupForm() {
-    console.log("Here");
     signupForm.classList.toggle("hide");
 }
 
