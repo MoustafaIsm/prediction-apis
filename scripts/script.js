bf490f95f30b30e6c1f77d055ca7e09b284bf6f3
Moustafa <moustafaism013@hotmail.com>
2022-09-11 18:51:41 +0300

    Cleaned more cothe code

--- scripts/script.js
+++ scripts/script.js
@@ -20,24 +20,36 @@ boredBtn.addEventListener("click", getTodoText);
 function submitUserName() {
     let name = userName.value;
     if (name != "") {
-        fetch("https://api.genderize.io?name=" + name)
-            .then((response) => response.json())
-            .then((data) => genderDisplay.textContent = data.gender);
-        fetch("https://api.agify.io/?name=" + name)
-            .then((response) => response.json())
-            .then((data) => ageDisplay.textContent = data.age);
-        fetch("https://api.nationalize.io/?name=" + name)
-            .then((response) => response.json())
-            .then((data) => {
-                if (data.country.length > 0) {
-                    if (data.country.length < 2) {
-                        nationalityDisplay.textContent = data.country[0];
-                    } else {
-                        nationalityDisplay.textContent = data.country[0].country_id + ", " + data.country[1].country_id;
-                    }
-                }
-            });
+        setGender();
+        setAge();
+        setNationality();
     }
+}
+
+function setGender() {
+    fetch("https://api.genderize.io?name=" + name)
+        .then((response) => response.json())
+        .then((data) => genderDisplay.textContent = data.gender);
+}
+
+function setAge() {
+    fetch("https://api.agify.io/?name=" + name)
+        .then((response) => response.json())
+        .then((data) => ageDisplay.textContent = data.age);
+}
+
+function setNationality() {
+    fetch("https://api.nationalize.io/?name=" + name)
+        .then((response) => response.json())
+        .then((data) => {
+            if (data.country.length > 0) {
+                if (data.country.length < 2) {
+                    nationalityDisplay.textContent = data.country[0];
+                } else {
+                    nationalityDisplay.textContent = data.country[0].country_id + ", " + data.country[1].country_id;
+                }
+            }
+        });
 }
 
 function getTodoText() {
